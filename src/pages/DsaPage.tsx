b8adc2439ef61c9092a6fccb719844e6042813b6
SamarthSRao <Samarthz0901@gmail.com>
2026-04-16 12:26:29 +0530

    add level 5 practice problems and UI updates

--- src/pages/DsaPage.tsx
+++ src/pages/DsaPage.tsx
@@ -91,6 +91,7 @@ export default function DsaPage() {
       case 1: return <Brain className="w-5 h-5" />;
       case 2: return <Rocket className="w-5 h-5" />;
       case 3: return <Globe className="w-5 h-5" />;
+      case 4: return <Library className="w-5 h-5" />;
       default: return <Binary className="w-5 h-5" />;
     }
   };
